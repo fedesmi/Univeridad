7031d1d4e939ed4492fe44f7c09f0713cc57e7a2
fmichel <fmichel@FU-D-PRD-SMG-LB.brightstarcorp.com>
2017-05-12 20:59:26 -0300

    lista empleados y crear nuevo empleado

---
+++ src/java/com/controladores/VariablesBean.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.controladores;
+
+import com.entidades.Empleado;
+import com.entidades.Variable;
+import com.repositorios.VariableFacade;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import org.primefaces.event.RowEditEvent;
+
+/**
+ *
+ * @author fmichel
+ */
+@Named(value = "variablesBean")
+@RequestScoped
+public class VariablesBean {
+
+     @Inject
+    private VariableFacade variableFacade;
+     
+    private List<Variable> variables;
+    /**
+     * Creates a new instance of VariablesBean
+     */
+    public VariablesBean() {
+    }
+    
+    // This is the required method to get the datatable list.
+    @PostConstruct
+    public void init() {
+        variables= variableFacade.findAll();
+    }
+
+
+    /**
+     * @return the variableFacade
+     */
+    public VariableFacade getVariableFacade() {
+        return variableFacade;
+    }
+
+    /**
+     * @param variableFacade the variableFacade to set
+     */
+    public void setVariableFacade(VariableFacade variableFacade) {
+        this.variableFacade = variableFacade;
+    }
+
+    /**
+     * @return the variables
+     */
+    public List<Variable> getVariables() {
+        return variables;
+    }
+
+    /**
+     * @param variables the variables to set
+     */
+    public void setVariables(List<Variable> variables) {
+        this.variables = variables;
+    }
+    
+        public void onRowEdit(RowEditEvent event)  {
+        Variable var = (Variable) event.getObject();
+        this.variableFacade.edit(var);
+        //GuardarEdicion(empVar);
+        FacesMessage msg = new FacesMessage("Variable Editada", String.valueOf(var.getDescripcion()));
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+     
+    public void onRowCancel(RowEditEvent event) {
+        Variable var = (Variable) event.getObject();
+        FacesMessage msg = new FacesMessage("Edición Cancelada", String.valueOf(var.getDescripcion()));
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+    
+}
---
+++ src/java/com/entidades/Variable.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.entidades;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author fmichel
+ */
+@Entity
+@Table(name = "Variable")
+public class Variable implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @Column(name = "descripcion")
+    private String descripcion;
+    @Column(name = "valor")
+    private String valor;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Variable)) {
+            return false;
+        }
+        Variable other = (Variable) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.entidades.Variables[ id=" + id + " ]";
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * @return the valor
+     */
+    public String getValor() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+    
+}
